501f3689b0cf8e2066e7b0f4c009d4f3616c214b
Flavius Dolha <flaviusd15@gmail.com>
2020-05-17 12:10:33 +0300

    Add register / login functionality

--- app.js
+++ app.js
@@ -1,14 +1,88 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const app = express();
+
+app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(session({
+  secret: "yosemitesecretkey",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = 3000;
 
-app.use(express.static('public'))
+mongoose.connect("mongodb://localhost:27017/Yosemite", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema ({
+  fullName: String,
+  username: String,
+  password: String,
+  storage: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  if (req.isAuthenticated()) {
+    res.redirect("/s");
+  } else {
+    res.sendFile(__dirname + "/index.html");
+  }
 });
 
-app.listen(PORT, function() {
-  console.log("Server working on port " + PORT);
+app.get("/s", function(req, res) {
+  if (req.isAuthenticated()) {
+    res.sendFile(__dirname +"/storage.html");
+  }
+  else {
+    res.redirect("/");
+  }
 });
+
+app.post("/register", function(req, res) {
+  User.register({fullName: req.body.fullName, username: req.body.username, storage: "a"} , req.body.password, function(err, user) {
+    if (err) {
+       console.log(err)
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        return res.status(200).send({result: 'redirect', url:'/s'})
+      });
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  req.login(user, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function() {
+        return res.status(200).send({result: 'redirect', url:'/s'})
+      });
+    }
+  });
+});
+
+app.listen(PORT);
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -7,4 +7,72 @@ $(document).ready(function() {
     $("#login").addClass("hide");
     $("#register").removeClass("hide");
   });
+
+  $("#registerName").focus(function() {
+    $("#registerName").removeClass("input-invalid");
+    $("#underFullNameRegister").addClass("hide");
+  });
+
+  $("#registerEmail").focus(function() {
+    $("#registerEmail").removeClass("input-invalid");
+    $("#underEmailRegister").addClass("hide");
+  });
+
+  $("#registerPassword").focus(function() {
+    $("#registerPassword").removeClass("input-invalid");
+    $("#underPasswordRegister").addClass("hide");
+  });
+
+  $("#loginEmail").focus(function() {
+    $("#loginEmail").removeClass("input-invalid");
+    $("#underEmailLogin").addClass("hide");
+  });
+
+  $("#loginPassword").focus(function() {
+    $("#loginPassword").removeClass("input-invalid");
+    $("#underPasswordLogin").addClass("hide");
+  });
+
+  $("#registerButton").click(function() {
+    var inputIsOk = true;
+    if ($("#registerName").val().length < 3) {
+      inputIsOk = false;
+      $("#registerName").addClass("input-invalid");
+      $("#underFullNameRegister").removeClass("hide");
+      $("#underFullNameRegister").addClass("text-invalid");
+      $("#underFullNameRegister").text("Your name should be at least 3 characters.");
+    }
+
+    if ($("#registerEmail").val().length < 3) {
+      inputIsOk = false;
+      $("#registerEmail").addClass("input-invalid");
+      $("#underEmailRegister").removeClass("hide");
+      $("#underEmailRegister").addClass("text-invalid");
+      $("#underEmailRegister").text("Your email is not valid.");
+    }
+
+    if ($("#registerPassword").val().length < 8) {
+      inputIsOk = false;
+      $("#registerPassword").addClass("input-invalid");
+      $("#underPasswordRegister").removeClass("hide");
+      $("#underPasswordRegister").addClass("text-invalid");
+      $("#underPasswordRegister").text("The password should contain at least 8 characters.");
+    }
+
+    if (inputIsOk) {
+      $.post("http://localhost:3000/register", {fullName: $("#registerName").val(), username: $("#registerEmail").val(), password: $("#registerPassword").val()}, function(response) {
+        if (response.result == "redirect") {
+          window.location.replace(response.url);
+        }
+      });
+    }
+  });
+
+  $("#loginButton").click(function() {
+    $.post("http://localhost:3000/login", {username: $("#loginEmail").val(), password: $("#loginPassword").val()}, function(response) {
+      if (response.result == "redirect") {
+        window.location.replace(response.url);
+      }
+    });
+  });
 });
