37caae071013336ad45af339863fc804cd79f5c0
Flavius Dolha <flaviusdolha@icloud.com>
2021-01-15 01:03:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 const PORT = process.env.PORT || 3000;
 const URL  = process.env.MONGODB_URL || "mongodb+srv://yosemiteadmin:yosemitepassword@yosemite.6rn6p.mongodb.net/Yosemite?retryWrites=true&w=majority";
