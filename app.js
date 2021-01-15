d3f3235197d02a9d494f4323bc8761575cfa8287
Flavius Dolha <flaviusdolha@icloud.com>
2021-01-15 15:28:32 +0200

    add cors

--- app.js
+++ app.js
@@ -11,16 +11,11 @@ const multer = require("multer");
 const fs = require("fs");
 const moveFile = require("move-file");
 const getSize = require('get-folder-size');
+const cors = require('cors');
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+app.use(cors());
 app.use(express.static('public'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended:true }));
